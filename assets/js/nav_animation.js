67c5115d71ea580064367ea827921bfced425c17
Matin <moh.matinfadaei@gmail.com>
2023-07-16 21:44:29 +0430

    first add in modal

---
+++ assets/js/nav_animation.js
@@ -0,0 +1,20 @@
+function myfunction(){
+    aler=document.getElementById("aler") 
+    bac=document.getElementById("bac") 
+  
+  
+  
+    aler.style.transform="translatey(0px)"
+    bac.style.visibility="visible"
+  }
+  
+function myfunction1(){
+    aler=document.getElementById("aler") 
+    bac=document.getElementById("bac") 
+  
+  
+  
+    aler.style.transform="translatey(1000px)"
+    bac.style.visibility="hidden"
+  }
+  
